533c8217f2516455c92348aef42dde25efeefd23
gray-snyk <graham.theodore@snyk.io>
2025-12-02 13:29:14 -0500

    plain -> html

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/profile', (req, res) => {
   let username = req.query.username || 'Guest';
   
   
-   res.contentType('text/plain').send(`<h1>Welcome, ${username}</h1><script>alert('XSS!');</script>`);
+   res.contentType('text/html').send(`<h1>Welcome, ${username}</h1><script>alert('XSS!');</script>`);
  });
 
 
